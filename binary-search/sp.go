b79e345046cc0fcae0ca46c5364cb3bdade306ce
vishalmane9196 <vishalmane9196@gmail.com>
2022-11-16 06:01:12 +0530

    Added [Leetcode-374] problem

--- binary-search/sp.go
+++ binary-search/sp.go
@@ -278,3 +278,23 @@ func maxi(arr []int) int {
 	}
 	return max
 }
+
+//Link : https://leetcode.com/problems/guess-number-higher-or-lower/
+func guessNumber(n int) int {
+
+	left := 1
+	right := n
+
+	for left <= right {
+		mid := (left + right) / 2
+
+		if guess(mid) == 0 {
+			return mid
+		} else if guess(mid) == 1 {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return 0
+}
