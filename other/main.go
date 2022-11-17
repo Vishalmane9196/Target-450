d912341dceb6c75129057ee32be8bbcf4500f54c
vishalmane9196 <vishalmane9196@gmail.com>
2022-11-17 07:54:50 +0530

    Added [Leetcode-223] problem

--- other/main.go
+++ other/main.go
@@ -4,7 +4,18 @@ import "fmt"
 
 func main() {
 
-	number := 9669
-	res := maximum69Number(number)
-	fmt.Println("maximum69Number : ", res)
+	// number := 9669
+	// res := maximum69Number(number)
+	// fmt.Println("maximum69Number : ", res)
+
+	ax1 := -3
+	ay1 := 0
+	ax2 := 3
+	ay2 := 4
+	bx1 := 0
+	by1 := -1
+	bx2 := 9
+	by2 := 2
+	res := computeArea(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2)
+	fmt.Println("computeArea : ", res)
 }
--- other/other.go
+++ other/other.go
@@ -20,3 +20,44 @@ func maximum69Number(num int) int {
 
 	return ans
 }
+
+//Link: https://leetcode.com/problems/rectangle-area/
+func computeArea(ax1 int, ay1 int, ax2 int, ay2 int, bx1 int, by1 int, bx2 int, by2 int) int {
+
+	first := area(ax1, ay1, ax2, ay2)
+	second := area(bx1, by1, bx2, by2)
+
+	leftOverlap := max(ax1, bx1)
+	downOverlap := max(ay1, by1)
+
+	rightOverlap := min(ax2, bx2)
+	upOverlap := min(ay2, by2)
+
+	overlap := area(leftOverlap, downOverlap, rightOverlap, upOverlap)
+
+	return first + second - overlap
+
+}
+
+// Returns 0 if right < left or up < down
+func area(left, down, right, up int) int {
+	if left > right || down > up {
+		return 0
+	}
+	return (right - left) * (up - down)
+}
+
+// math.Max accepts and returns floats and makes a mess
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
