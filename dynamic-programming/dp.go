5725773d8180cab63b3a9900b851397e0f195456
vishalmane9196 <vishalmane9196@gmail.com>
2022-11-22 06:31:34 +0530

    Added [Leetcode-297] problem

--- dynamic-programming/dp.go
+++ dynamic-programming/dp.go
@@ -1,6 +1,25 @@
 package main
 
-/*
+import "math"
 
+//Link: https://leetcode.com/problems/perfect-squares/
+func numSquares(n int) int {
 
- */
+	dp := make([]int, n+1)
+
+	dp[0] = 0
+	dp[1] = 1
+
+	for i := 2; i <= n; i++ {
+
+		min := math.MaxInt
+		for j := 1; j*j <= i; j++ {
+			rem := i - j*j
+			if dp[rem] < min {
+				min = dp[rem]
+			}
+		}
+		dp[i] = min + 1
+	}
+	return dp[n]
+}
--- dynamic-programming/main.go
+++ dynamic-programming/main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	/********Problems********/
+	number := 12
+	res := numSquares(number)
+	fmt.Printf("perfect square of %v  : %v\n", number, res)
 }
