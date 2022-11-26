911ae503837ee53c6c0e6f8ad64eb4b65313a575
vishalmane9196 <vishalmane9196@gmail.com>
2022-11-26 07:02:47 +0530

    Added [Leetcode-1235] problem

--- dynamic-programming/dp.go
+++ dynamic-programming/dp.go
@@ -1,8 +1,11 @@
 package main
 
-import "math"
+import (
+	"math"
+	"sort"
+)
 
-//Link: https://leetcode.com/problems/perfect-squares/
+// Link: https://leetcode.com/problems/perfect-squares/
 func numSquares(n int) int {
 
 	dp := make([]int, n+1)
@@ -22,4 +25,58 @@ func numSquares(n int) int {
 		dp[i] = min + 1
 	}
 	return dp[n]
+}
+
+/*
+Link: https://leetcode.com/problems/maximum-profit-in-job-scheduling/
+Link: https://www.youtube.com/watch?v=rZLvA1rsLy4&ab_channel=TanishqChaudhary
+*/
+type t struct {
+	s int
+	e int
+	p int
+}
+
+func jobScheduling(st []int, et []int, p []int) int {
+	r := make([]t, len(st))
+	for i := range st {
+		r[i] = t{s: st[i], e: et[i], p: p[i]}
+	}
+
+	sort.Slice(r, func(i, j int) bool { return r[i].s <= r[j].s })
+
+	var dp func(int) int
+	dp = func(i int) int {
+		if i >= len(r) {
+			return 0
+		}
+
+		a := r[i].p
+		for j := i + 1; j < len(r); j++ {
+			if r[i].e <= r[j].s {
+				a += dp(j)
+				break
+			}
+		}
+		na := dp(i + 1)
+		if na > a {
+			return na
+		}
+		return a
+	}
+	dp = c(dp)
+	return dp(0)
+}
+
+func c(f func(int) int) func(int) int {
+	var m = make(map[int]int)
+	d := func(arg int) int {
+		if val, ok := m[arg]; ok {
+			return val
+		}
+		val := f(arg)
+		m[arg] = val
+		return val
+	}
+	return d
 }
--- dynamic-programming/main.go
+++ dynamic-programming/main.go
@@ -4,7 +4,14 @@ import "fmt"
 
 func main() {
 	/********Problems********/
-	number := 12
-	res := numSquares(number)
-	fmt.Printf("perfect square of %v  : %v\n", number, res)
+	// number := 12
+	// res := numSquares(number)
+	// fmt.Printf("perfect square of %v  : %v\n", number, res)
+
+	startTime := []int{1, 2, 3, 3}
+	endTime := []int{3, 4, 5, 6}
+	profit := []int{50, 10, 40, 70}
+	res := jobScheduling(startTime, endTime, profit)
+	fmt.Printf("jobScheduling  : %v\n", res)
+
 }
