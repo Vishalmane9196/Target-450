03fb4e42c7f86c6cfc4277c63ff11131c461f854
vishalmane9196 <vishalmane9196@gmail.com>
2022-12-04 13:26:38 +0530

    Added [Leetcode-2256] problem

--- other/other.go
+++ other/other.go
@@ -149,3 +149,26 @@ func (this *RandomizedSet) GetRandom() int {
 	i := rand.Intn(len(this.elementlist))
 	return this.elementlist[i]
 }
+
+// Link: https://leetcode.com/problems/minimum-average-difference/
+func minimumAverageDifference(nums []int) int {
+	n, total := len(nums), 0
+	for _, num := range nums {
+		total += num
+	}
+	min, prefix, idx := total, 0, 0
+	for i := 0; i < n-1; i++ {
+		prefix += nums[i]
+		cur := prefix/(i+1) - (total-prefix)/(n-i-1)
+		if cur < 0 {
+			cur = -cur
+		}
+		if cur < min {
+			min, idx = cur, i
+		}
+	}
+	if total/n < min {
+		return n - 1
+	}
+	return idx
+}
