7a3f62593e929bb85493ad0fcdf3a89c3e4d5215
vishalmane9196 <vishalmane9196@gmail.com>
2022-12-12 07:22:58 +0530

    Added [Leetcode-70] Problem

--- other/other.go
+++ other/other.go
@@ -172,3 +172,18 @@ func minimumAverageDifference(nums []int) int {
 	}
 	return idx
 }
+
+// Link: https://leetcode.com/problems/climbing-stairs/
+func climbStairs(n int) int {
+
+	dp := make([]int, n+1)
+	if n <= 2 {
+		return n
+	}
+	dp[0], dp[1], dp[2] = 0, 1, 2
+
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
