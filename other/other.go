d5bd1ec52b660b96b1da30ab2bc5ff03f081c6d9
vishalmane9196 <vishalmane9196@gmail.com>
2022-12-20 08:52:30 +0530

    Added [Leetcode-841] problem

--- other/other.go
+++ other/other.go
@@ -233,3 +233,27 @@ func evalRPN(tokens []string) int {
 	}
 	return stack[len(stack)-1]
 }
+
+// Link: https://leetcode.com/problems/keys-and-rooms/
+func canVisitAllRooms(rooms [][]int) bool {
+	if rooms == nil || len(rooms) == 0 {
+		return false
+	}
+
+	mp := make(map[int]bool)
+	check(rooms, 0, mp)
+
+	return len(mp) == len(rooms)
+}
+
+func check(rooms [][]int, room int, mp map[int]bool) {
+	if _, ok := mp[room]; ok {
+		return
+	}
+
+	mp[room] = true
+
+	for _, r := range rooms[room] {
+		check(rooms, r, mp)
+	}
+}
